f01159cf7b9b2ff2b3d8eaeca435ca1432ec5f86
Jud Saoud <jud@p.foundation>
2023-10-12 13:35:49 +0100

    Updated temporary image holder

--- src/pages/initiatives.tsx
+++ src/pages/initiatives.tsx
@@ -8,7 +8,7 @@ const projects: ProjectData[] = [
   {
     title: "LebanonAlerts",
     description: `Established in the wake of the 6 February 2023 Turkey-Syria earthquake, "Lebanon Alerts" is a X account committed to bridging the information gap experienced by the Lebanese populace. Born from the pressing need for transparency and timely updates, the account integrates with Raspberry Shake and global seismographic networks, delivering real-time seismic activity reports. Additionally, a dedicated YouTube live stream offers a continuous visual of the seismographs, ensuring data transparency and public awareness.`,
-    image: require(`${assetsDir}/lebanonAlerts.png`),
+    image: require(`${assetsDir}/80860929.png`),
     role: "Active",
     x: "LebanonAlerts",
     youtube: "LebanonAlerts",
@@ -16,27 +16,26 @@ const projects: ProjectData[] = [
   {
     title: "Lebanon IA Monitor",
     description: `A pivotal initiative aimed at maintaining transparency in Lebanon's internet landscape. Recognizing that ISPs often refrain from acknowledging outages, especially when attributed to maintenance or sourcing lapses, this platform diligently monitors internet access across various providers. Offering real-time outage reports, it harnesses data from diverse sources such as real-time traffic to partner websites/apps, RIPE Atlas, and a network of mobile devices strategically placed throughout Lebanon.`,
-    image: require(`${assetsDir}/iam.png`),
+    image: require(`${assetsDir}/80860929.png`),
     role: "Active",
     x: "LebanonIAMonitor",
   },
   {
     title: "PTUN",
     description: ``,
-    url: "https:///",
-    image: require(`${assetsDir}/ptun.png`),
+    image: require(`${assetsDir}/80860929.png`),
     role: "Active",
   },
   {
     title: "Open Reporting",
     description: ``,
-    image: require(`${assetsDir}/or.png`),
+    image: require(`${assetsDir}/80860929.png`),
     role: "Queued",
   },
   {
     title: "National Digital Archive",
     description: ``,
-    image: require(`${assetsDir}/nda.png`),
+    image: require(`${assetsDir}/80860929.png`),
     role: "Queued",
   },
 ];
